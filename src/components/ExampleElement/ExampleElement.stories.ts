6b64e8579053baac365d392bb702f74158b6d6ca
Dan Valinotti <dvalinotti@icloud.com>
2021-06-25 10:58:13 -0400

    Add storybook to project

---
+++ src/components/ExampleElement/ExampleElement.stories.ts
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/web-components';
+import { html, TemplateResult } from 'lit-html';
+import { ExampleElementProps } from '../ExampleElement';
+import '../ExampleElement';
+
+export default {
+  title: 'Example/ExampleElement',
+  argTypes: {
+    name: {
+      control: 'text',
+      defaultValue: 'ExampleElement'
+    }
+  },
+} as Meta;
+
+export const Primary = (props: ExampleElementProps): TemplateResult<1> => html`<example-element name=${props.name}></example-element>`
