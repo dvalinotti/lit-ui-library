6abfb731b81d3fae1513fbfa2fa2f9354176e667
Dan Valinotti <dvalinotti@icloud.com>
2021-06-28 22:01:30 -0400

    Add ui progress tests

---
+++ src/components/UIProgress/UIProgress.test.js
@@ -0,0 +1,28 @@
+import {fixture, expect, html} from '@open-wc/testing';
+// import {html} from 'lit/static-html.js';
+
+const getTestElement = async () => {
+  return fixture(
+    html`<ui-progress label="UIProgress" value="0"></ui-progress>`
+  );
+};
+
+describe('UIProgress tests', () => {
+  it('renders correctly', async () => {
+    const el = await getTestElement();
+
+    expect(el.tagName.toLowerCase()).to.equal('ui-progress');
+  });
+
+  // it('update progress value', async () => {
+  //   const el = await getTestElement();
+
+  //   el.setAttribute('value', '25');
+
+  //   console.log(el);
+
+  //   let barFill = el.shadowRoot;
+  //   console.log(el.shadowRoot);
+  //   expect(barFill.clientWidth).to.be.greaterThan(0);
+  // });
+});
