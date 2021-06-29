20d5712deebb43a51a796a5c92387c0e86d13ea0
Dan Valinotti <dvalinotti@icloud.com>
2021-06-29 13:13:46 -0400

    web component manifest

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export {default as UIButton} from './components/UIButton/UIButton';
 export {default as UIModal} from './components/UIModal/UIModal';
+export {default as UICard} from './components/UICard/UICard';
